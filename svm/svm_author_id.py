0b4a39883d6ebd1f86ef224ccc05ad57b9fceca8
Daniel Shank <Daniel Shank>
2016-06-13 18:02:51 -0400

    Add training and prediction run times to nb and svm within comments

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -19,6 +19,9 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
+#########################################################
+### your code goes here ###
+
 from sklearn.svm import SVC
 clf = SVC(kernel='linear')
 
@@ -27,16 +30,10 @@ labels_train = labels_train[:len(labels_train) / 100]
 
 t0 = time()
 clf.fit(features_train, labels_train)
-print "training time:", round(time() - t0, 3), "s"
+print "training time:", round(time() - t0, 3), "s" #training time found to be approx. 0.083 s
 t0 = time()
 clf.predict(features_test)
-print "prediction time:", round(time() - t0, 3), "s"
+print "prediction time:", round(time() - t0, 3), "s" #prediction time found to be approx. 0.879 s
 print clf.score(features_test, labels_test)
 
-
 #########################################################
-### your code goes here ###
-
-#########################################################
-
-
