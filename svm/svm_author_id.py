ac796d5215458ae5c9977d04c0773263f31da37b
Daniel Shank <Daniel Shank>
2016-06-13 18:50:11 -0400

    Revert SVM to use entire data set for training

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -25,9 +25,6 @@ features_train, features_test, labels_train, labels_test = preprocess()
 from sklearn.svm import SVC
 clf = SVC(C=10000., kernel='rbf')
 
-features_train = features_train[:len(features_train) / 100]
-labels_train = labels_train[:len(labels_train) / 100]
-
 t0 = time()
 clf.fit(features_train, labels_train)
 print "training time:", round(time() - t0, 3), "s"
