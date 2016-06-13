3849b3cdae39469c8d70556fa392e6062fcc0427
Daniel Shank <Daniel Shank>
2016-06-13 18:09:02 -0400

    Add training and test times to NB within comments

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -26,15 +26,17 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+
 from sklearn.naive_bayes import GaussianNB
 clf = GaussianNB()
 t0 = time()
 clf.fit(features_train, labels_train)
-print "training time:", round(time()-t0, 3), "s"
+print "training time:", round(time()-t0, 3), "s" #training time approx. 1.275 s
 t0 = time()
 clf.predict(features_test)
-print "prediction time:", round(time()-t0, 3), "s"
+print "prediction time:", round(time()-t0, 3), "s" #prediction time approx. 0.201 s
 print clf.score(features_test, labels_test)
+
 #########################################################
 
 
