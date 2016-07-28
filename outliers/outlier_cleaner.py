fe94305d0c14788c84857844441ec3eb35fae42f
Daniel Shank <Daniel Shank>
2016-07-28 10:58:54 -0400

    Add code which cleans away 10% of data with the worst errors Add code which gives before and after slopes and scores of the linear model

--- outliers/outlier_cleaner.py
+++ outliers/outlier_cleaner.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-
+import numpy
 
 def outlierCleaner(predictions, ages, net_worths):
     """
@@ -9,12 +9,33 @@ def outlierCleaner(predictions, ages, net_worths):
 
         Return a list of tuples named cleaned_data where 
         each tuple is of the form (age, net_worth, error).
-    """
-    
-    cleaned_data = []
+    """    
+    e2 = (net_worths - predictions)**2
+    errors = net_worths - predictions
+    #cleaned_data = numpy.array([ages, networths, errors])
+    set_size = len(ages)
 
     ### your code goes here
+    #for deletion in int(0.1 * set_size):
+    #    index = numpy.argmax(cleaned_data, axis=1)[2]
+    #    cleaned_data = numpy.delete(cleaned_data, index, axis = 1)
 
-    
+    cleaned_data = []
+
+    for addition in range(int(0.9 * set_size)):
+        index = numpy.argmin(e2)
+        cleaned_data.append([ages[index], net_worths[index], errors[index]])
+        ages = numpy.delete(ages, index)
+        net_worths = numpy.delete(net_worths, index)
+        errors = numpy.delete(errors, index)
+        e2 = numpy.delete(e2, index)
+
     return cleaned_data
 
+#a = np.arange(30).reshape(3, 10)
+#print np.argmax(a, axis=1)[0]
+#a = np.delete(a, 9, axis=1)
+#print a
+
+#a = numpy.arange(30).reshape(3, 10)
+#print outlierCleaner(a[0], a[1], a[2])
--- outliers/outlier_removal_regression.py
+++ outliers/outlier_removal_regression.py
@@ -26,13 +26,13 @@ ages_train, ages_test, net_worths_train, net_worths_test = train_test_split(ages
 ### fill in a regression here!  Name the regression object reg so that
 ### the plotting code below works, and you can see what your regression looks like
 
+from sklearn.linear_model import LinearRegression
 
+reg = LinearRegression()
+reg.fit(ages_train, net_worths_train)
 
-
-
-
-
-
+print "The slope of the regression is %f" % reg.coef_
+print "The score of the regression on the test data is %f" % reg.score(ages_test, net_worths_test)
 
 
 
@@ -82,3 +82,5 @@ if len(cleaned_data) > 0:
 else:
     print "outlierCleaner() is returning an empty list, no refitting to be done"
 
+print "The new slope of the regression is %f" % reg.coef_
+print "The new score of the regression on the test data is %f" % reg.score(ages_test, net_worths_test)
