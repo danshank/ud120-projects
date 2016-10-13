8c406af6e16283b69277b3fd64d087d823d52b9c
Daniel Shank <Daniel Shank>
2016-10-13 14:27:20 -0400

    Change train_test_split so that 30 percent of data is used for testing

--- validation/validate_poi.py
+++ validation/validate_poi.py
@@ -32,7 +32,7 @@ labels, features = targetFeatureSplit(data)
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.cross_validation import train_test_split
 
-features_train, features_test, labels_train, labels_test = train_test_split(features, labels, random_state=42)
+features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size=0.3, random_state=42)
 
 clf = DecisionTreeClassifier()
 clf.fit(features_train, labels_train)
